f461449dad8c09e4d83c6eb72ae2ec183e51a2cd
zelalemgetahun9374 <zelalemgetahun9374@gmail.com>
2021-07-27 21:50:47 +0300

    feat: helper scripts added

---
+++ scripts/csv_helper.py
@@ -0,0 +1,26 @@
+import os
+import sys
+import pandas as pd
+from app_logger import App_Logger
+
+class CsvHelper():
+
+    def __init__(self):
+        self.logger = App_Logger().get_logger(__name__)
+
+    def save_csv(self, df, csv_path, index=False):
+        try:
+            df.to_csv(csv_path, index=index)
+            self.logger.info(f'Csv file saved in {csv_path}')
+
+        except Exception:
+            self.logger.exception('File saving failed.')
+
+    def read_csv(self, csv_path, missing_values=["n/a", "na", "undefined"]):
+        try:
+            df = pd.read_csv(csv_path, na_values=missing_values)
+            self.logger.info(f'Csv file read from {csv_path}')
+            return df
+
+        except FileNotFoundError:
+            self.logger.exception('File not found.')
