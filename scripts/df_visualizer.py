c261c166d1d7350829b847f25a128f4ddf29b2e5
zelalemgetahun9374 <zelalemgetahun9374@gmail.com>
2021-07-30 00:07:21 +0300

    feat: plotting functions upgraded

--- scripts/df_visualizer.py
+++ scripts/df_visualizer.py
@@ -5,21 +5,25 @@ import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
-def plotly_plot_pie(df, column, limit=None):
+def plotly_plot_pie(df, column, limit=None, title=None):
     a = pd.DataFrame({'count': df.groupby([column]).size()}).reset_index()
     a = a.sort_values("count", ascending=False)
     if limit:
         a.loc[a['count'] < limit, column] = f'Other {column}s'
-    fig = px.pie(a, values='count', names=column, title=f'Distribution of {column}s', width=800, height=500)
+    if title == None:
+        title=f'Distribution of {column}'
+    fig = px.pie(a, values='count', names=column, title=title, width=800, height=500)
     fig.show()
 
-def plotly_plot_hist(df, column, color=['cornflowerblue']):
+def plotly_plot_hist(df, column, color=['cornflowerblue'], title=None):
+    if title == None:
+        title=f'Distribution of {column}'
     fig = px.histogram(
             df,
             x=column,
             marginal='box',
             color_discrete_sequence=color,
-            title=f'Distribution of {column}')
+            title=title)
     fig.update_layout(bargap=0.01)
     fig.show()
 
