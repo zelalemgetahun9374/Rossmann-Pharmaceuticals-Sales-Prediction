4aa8956ece9fd86d9670837529b48971c193ea6e
zelalemgetahun9374 <zelalemgetahun9374@gmail.com>
2021-07-31 16:21:07 +0300

    feat: config updated

--- scripts/config.py
+++ scripts/config.py
@@ -4,7 +4,7 @@ class Config:
   RANDOM_SEED = 90
   ASSETS_PATH = Path("../")
   REPO = "~/Documents/Rossmann-Pharmaceuticals-Sales-Prediction"
-  DATASET_FILE_PATH = "data/merged.csv"
+  DATASET_FILE_PATH = "data/train.csv"
   DATASET_PATH = ASSETS_PATH / "data"
   FEATURES_PATH = ASSETS_PATH / "features"
   MODELS_PATH = ASSETS_PATH / "models"
