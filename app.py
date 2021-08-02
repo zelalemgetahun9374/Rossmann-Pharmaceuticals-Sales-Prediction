565d6f085b26c01c494a942794c2d77d573635f5
zelalemgetahun9374 <zelalemgetahun9374@gmail.com>
2021-08-03 00:53:04 +0300

    feat: updated streamlit app

--- app.py
+++ app.py
@@ -1,17 +1,44 @@
 import os
 import sys
+
+# import the child scripts
 import streamlit as st
+import awesome_streamlit as ast
+import src.pages.home
+import src.pages.data
+import src.pages.pred
+import src.pages.insights
 
-sys.path.append(os.path.abspath(os.path.join('./pages')))
+ast.core.services.other.set_logging_format()
 
-import page1
-import page2
-
+# create the pages
 PAGES = {
-  "Home": page1,
-  "Dashboard": page2,
+    "Home": src.pages.home,
+    "Data":src.pages.data,
+    "Insights": src.pages.insights,
+    "Run Predictions": src.pages.pred
 }
 
-selection = st.sidebar.radio("Go to page", list(PAGES.keys()))
-page = PAGES[selection]
-page.app()
+
+# render the pages
+def main():
+    """Main function of the App"""
+    st.sidebar.title("Navigation")
+    selection = st.sidebar.radio("Go to", list(PAGES.keys()))
+
+    page = PAGES[selection]
+
+    with st.spinner(f"Loading {selection} ..."):
+        ast.shared.components.write_page(page)
+
+    st.sidebar.title("About")
+    st.sidebar.info(
+        """
+        This App is an end-to-end product that enables the Rosemann pharmaceutical company to 
+        view predictions on sales across their stores and 6 weeks ahead of time and the trends expected.
+"""
+    )
+
+# run it
+if __name__ == "__main__":
+    main()
