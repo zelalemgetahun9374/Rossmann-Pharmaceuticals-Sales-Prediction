7f30483ddf3b365d73de3515997813a99adb096a
zelalemgetahun9374 <zelalemgetahun9374@gmail.com>
2021-08-02 09:37:10 +0300

    chore: models and other files added

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import os
 import sys
+import streamlit as st
 
 sys.path.append(os.path.abspath(os.path.join('./pages')))
 
 import page1
 import page2
-import streamlit as st
 
 PAGES = {
   "Home": page1,
